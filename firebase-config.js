76f92bb96f52763223c65c851f3aa7d30e6a904f
matukwo <matukwo@gmail.com>
2026-03-09 02:26:18 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,21 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore, collection, addDoc, query, where, orderBy, onSnapshot } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// REEMPLAZA ESTO CON TUS DATOS REALES DE LA CONSOLA DE FIREBASE
+// Reemplaza estos datos con los de tu Consola de Firebase -> Configuración del proyecto
 const firebaseConfig = {
-  apiKey: "AIzaSyA...", 
+  apiKey: "TU_API_KEY_REAL",
   authDomain: "tu-proyecto.firebaseapp.com",
   projectId: "tu-proyecto",
   storageBucket: "tu-proyecto.appspot.com",
   messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  appId: "tu-app-id"
 };
 
-// Inicializar Firebase
+// Inicializamos Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Exportar a la ventana global para que pelicula.html pueda ver estas funciones
+// Compartimos las funciones con el resto de tus archivos (pelicula.html)
 window.db = db;
 window.addDoc = addDoc;
 window.collection = collection;
