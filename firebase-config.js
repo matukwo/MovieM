42f307d175b657540fedf033da43428c531f85b2
matukwo <matukwo@gmail.com>
2026-03-09 03:11:17 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,20 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, collection, addDoc, query, where, orderBy, onSnapshot } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { 
+    getFirestore, 
+    collection, 
+    addDoc, 
+    query, 
+    where, 
+    orderBy, 
+    onSnapshot, 
+    doc, 
+    getDoc, 
+    setDoc, 
+    arrayUnion, 
+    arrayRemove 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// Reemplaza estos datos con los de tu Consola de Firebase -> Configuración del proyecto
-  const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyAn5N7m7EhICK2aZG4Nx8DIW9RZd9kK1DA",
     authDomain: "moviem-5b73d.firebaseapp.com",
     projectId: "moviem-5b73d",
@@ -10,13 +22,12 @@ import { getFirestore, collection, addDoc, query, where, orderBy, onSnapshot } f
     messagingSenderId: "843796138544",
     appId: "1:843796138544:web:c2bde9b39d437b84d97bf5",
     measurementId: "G-7FH2FMPYZT"
-  };
+};
 
-// Inicializamos Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Compartimos las funciones con el resto de tus archivos (pelicula.html)
+// Exportamos todo al objeto window para que pelicula.html pueda usarlo
 window.db = db;
 window.addDoc = addDoc;
 window.collection = collection;
@@ -24,3 +35,10 @@ window.query = query;
 window.where = where;
 window.orderBy = orderBy;
 window.onSnapshot = onSnapshot;
+
+// --- NUEVAS FUNCIONES PARA LISTAS PERSONALIZADAS ---
+window.doc = doc;
+window.getDoc = getDoc;
+window.setDoc = setDoc;
+window.arrayUnion = arrayUnion;
+window.arrayRemove = arrayRemove;
